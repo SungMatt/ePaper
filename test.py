01d64461b7b86733b7116ae15b62e6af7efb98c3
Matthew Sung <SungMatt@SungMatt.com>
2022-11-20 17:32:22 +0800

    Add new tests

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-from IT8951.IT8951.display import AutoEPDDisplay
-from IT8951.IT8951 import constants
+from IT8951.display import AutoEPDDisplay
+from IT8951.display import VirtualEPDDisplay
+from IT8951 import constants
 from PIL import Image
+from time import sleep
 
 V_COM = -1.77
 
-def main(path):
-    display = AutoEPDDisplay(vcom=V_COM)
+def main(path, virtual=False):
+    if virtual:
+        display = VirtualEPDDisplay(vcom=V_COM)
+    else:
+        display = AutoEPDDisplay(vcom=V_COM)
+
     # Make display clear
 
     display.clear()
@@ -19,4 +25,10 @@ def main(path):
     # Display the image
     display.draw_full(constants.DisplayModes.GC16)
 
-main("IT8951/IT8951/test/integration/images/sleeping_penguin.png")
+def clear_screen():
+    display = AutoEPDDisplay(vcom=V_COM)
+    display.clear()
+
+main("images/Epcot.png")
+#sleep(10)
+
